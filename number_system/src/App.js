32462131821dffc389bca7b98cd1b9709cf72350
xdev200 <xdev200200@gmail.com>
2021-02-03 17:19:00 +0530

    Added: Google Analytics

--- number_system/src/App.js
+++ number_system/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import Landing from './components/Landing/Landing'
-
+import ReactGA from 'react-ga';
+function initializeReactGA() {
+  ReactGA.initialize('G-T4V19VQYFE');
+  ReactGA.pageview('/index');
+}
 function App() {
   return (
     <div className="App">
