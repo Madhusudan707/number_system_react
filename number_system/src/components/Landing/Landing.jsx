8853f96772af0ba22ed4c5bbd34454d67c025590
xdev200 <xdev200200@gmail.com>
2021-02-04 12:54:01 +0530

    Refactor: == to ===

--- number_system/src/components/Landing/Landing.jsx
+++ number_system/src/components/Landing/Landing.jsx
@@ -29,21 +29,21 @@ const Landing = () => {
     for (let i = 0; i < inputBox.length; i++) {
       let textBoxValue = 0;
       textBoxValue = document.getElementById(inputBox[i].id).value;
-      if (i == 0 && textBoxValue) {
+      if (i === 0 && textBoxValue) {
         //Binary to All
           axios.get(`http://127.0.0.1:3001/B2ALL/${textBoxValue}`).then((res) => {
           document.getElementById("decimal").value = res.data.resultB2D;
           document.getElementById("octal").value = res.data.resultB2O;
           document.getElementById("hexadecimal").value = res.data.resultB2HD;
         });
-      } else if (i == 1 && textBoxValue) {
+      } else if (i === 1 && textBoxValue) {
        //Decimal to All
        axios.get(`http://127.0.0.1:3001/D2ALL/${textBoxValue}`).then((res) => {
         document.getElementById("binary").value = res.data.resultD2B;
         document.getElementById("octal").value = res.data.resultD2O;
         document.getElementById("hexadecimal").value = res.data.resultD2HD;
       });
-      } else if (i == 2 && textBoxValue) {
+      } else if (i === 2 && textBoxValue) {
         //Octal to All
         axios.get(`http://127.0.0.1:3001/O2ALL/${textBoxValue}`).then((res) => {
         document.getElementById("binary").value = res.data.resultO2B;
@@ -51,7 +51,7 @@ const Landing = () => {
         document.getElementById("hexadecimal").value = res.data.resultO2HD;
       });
        
-      } else if (i == 3 && textBoxValue) {
+      } else if (i === 3 && textBoxValue) {
         //HexaDecimal to All
         axios.get(`http://127.0.0.1:3001/HD2ALL/${textBoxValue}`).then((res) => {
         document.getElementById("binary").value = res.data.resultHD2B;
